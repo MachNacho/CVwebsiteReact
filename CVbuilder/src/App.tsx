b7f5cc73b513b38b01642c1056ce8c8c27ffdb42
Asi <64479526+MachNacho@users.noreply.github.com>
2025-01-19 12:11:01 +0200

    Update App.tsx
    Add Home & Details page

--- CVbuilder/src/App.tsx
+++ CVbuilder/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
 import NavigationHeader from './components/NavigationBar/NavigationHeader'
+import HomePage from './components/HomePage/HomePage'
+import DetailsPage from './components/DetailsPage/DetailsPage'
 
 function App() {
 
   return (
     <>
       <NavigationHeader/>
+      <HomePage/>
+      <DetailsPage/>
     </>
   )
 }
